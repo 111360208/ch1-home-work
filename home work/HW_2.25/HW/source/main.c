6412e447ac90649637bba1c8ae2a23c9d52a0c6e
TT <t111360208@ntut.org.tw>
2022-10-06 21:28:01 +0800

    new

---
+++ home work/HW_2.25/HW/source/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("WWWWWWWWWWWW\n");
+	printf("W\n");
+	printf("  W			\n");
+	printf("    W       \n");
+	printf("       W    \n");
+	printf("         W \n");
+	printf("       W    \n");
+	printf("    W       \n");
+	printf("  W         \n");
+	printf("W\n");
+	printf("WWWWWWWWWWWW\n");
+	printf("    C  C\n");
+	printf(" C       C\n");
+	printf("C          C\n");
+	printf("C          C \n");
+	printf("\n");
+	printf("          T   \n");
+	printf("          T   \n");
+	printf("TTTTTTTTTTT   \n");
+	printf("          T   \n");
+	printf("          T   \n");
+	system("pause");
+	return 0;
+}
